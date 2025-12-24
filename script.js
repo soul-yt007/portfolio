d22a7b5f36077e8ba41afd4c8c3950713a231596
Soul-yt007 <alpha.hacker4927@gmail.com>
2025-12-24 18:43:42 +0530

    Seperately Updated

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute('href');
+    const targetSection = document.querySelector(targetId);
+
+    if (targetSection) {
+      targetSection.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
